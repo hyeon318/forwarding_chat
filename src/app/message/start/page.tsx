f0ef74122cb90e94163ef915894bd5d99c2709c8
spidey <kyurin123@gmail.com>
2024-03-26 23:39:22 +0900

    style added

--- src/app/message/start/page.tsx
+++ src/app/message/start/page.tsx
@@ -2,6 +2,7 @@
 import Button from "@/components/atom/button/Button";
 import Container from "@/components/atom/container/Container";
 import React from "react";
+import styles from "./page.module.css";
 
 export default function page() {
   const onClick = (
@@ -9,9 +10,18 @@ export default function page() {
   ) => {};
   return (
     <Container
-      style={{ height: "600px", marginTop: "30px", marginBottom: "30px" }}
+      style={{
+        height: "600px",
+        marginTop: "30px",
+        marginBottom: "30px",
+        display: "grid",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
     >
-      <Button onClick={e => onClick(e)}>카카오로 시작하기</Button>
+      <Button onClick={e => onClick(e)} className={styles.btnKakao}>
+        카카오로 시작하기
+      </Button>
     </Container>
   );
 }
